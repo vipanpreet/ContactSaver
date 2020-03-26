40140c7ead6ae871894955d2b60804f92fee1cd2
Viphacker <viphacker@Viphackers-iMac.local>
2020-03-26 03:39:53 -0700

    Added Navigation Router in Client

---
+++ client/src/Components/Pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p className='my-1'>This is MERN stack app for saving contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 1.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
