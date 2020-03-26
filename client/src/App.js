ae590cd4f94b6ff0b955ec6e498ca932016eea8f
Viphacker <viphacker@Viphackers-iMac.local>
2020-03-26 03:59:33 -0700

    Initalize Context API and Hooks

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import "./App.css";
 import Navbar from "./Components/Layout/Navbar";
 import Home from "./Components/Pages/Home";
 import About from "./Components/Pages/About";
+import ContactState from "./context/contact/ContactState";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment className='App'>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment className='App'>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
